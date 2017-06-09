c7f3f106e15af96464779dd55abde9ea53e88502
Matthew Levine <matthewlevine@Matthews-MacBook-Pro.local>
2017-06-09 09:48:04 -0700

    Moving files around

---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+Mura=require('core/mura');
+
+require('core/loader.js');
+require('core/core.js');
+require('core/cache.js');
+require('core/domselection.js');
+require('core/entity.js');
+require('core/entitycollection.js');
+require('core/feed.js');
+require('core/ui.js');
+require('core/form');
+require('core/templates');
+
+if(typeof window != 'undefined'){
+
+  window.Shadowbox=require('core/shadowbox');
+
+  window.m=Mura;
+  window.mura=Mura;
+  window.Mura=Mura;
+}
+
+module.exports=Mura;
