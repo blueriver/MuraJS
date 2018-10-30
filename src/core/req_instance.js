a55e72a6a1d1cfe7a22a25dbdc44add524a7aa3a
Matt Levine <matt.levine@blueriver.com>
2018-10-29 20:58:18 -0700

    Fixing issue with limitting debug output

--- src/core/req_instance.js
+++ src/core/req_instance.js
@@ -86,14 +86,16 @@ Mura.Request=Mura.Core.extend(
       return this.requestHeaders;
     },
     nodeRequest:function(params){
-
+			var debug=typeof Mura.debug != 'undefined' && Mura.debug;
       self=this;
 
       if(typeof this.requestObject != 'undefined'){
         params.headers['User-Agent']='MuraJS';
         if(typeof this.requestObject.headers['cookie'] != 'undefined'){
-          console.log('pre cookies:');
-          console.log(this.requestObject.headers['cookie']);
+					if(debug){
+						console.log('pre cookies:');
+	          console.log(this.requestObject.headers['cookie']);
+					}
           params.headers['Cookie']=this.requestObject.headers['cookie'];
         }
         if(typeof this.requestObject.headers['x-client_id'] != 'undefined'){
@@ -328,6 +330,7 @@ Mura.Request=Mura.Core.extend(
       }
     },
     xhrRequest:function(params){
+			var debug=typeof Mura.debug != 'undefined' && Mura.debug;
 
       for(var h in Mura.requestHeaders){
           if(Mura.requestHeaders.hasOwnProperty(h)){
@@ -393,7 +396,7 @@ Mura.Request=Mura.Core.extend(
 
                   params.success(data, req);
               } else {
-								if(typeof req.responseText != 'undefined'){
+								if(debug && typeof req.responseText != 'undefined'){
 									console.log(req.responseText);
 								}
 
