f2ca1924ec85bd9923155d5a21837ecc428462f8
Matt Levine <matt.levine@blueriver.com>
2019-01-08 09:14:31 -0800

    Working on allowing colleciton layouts to be implemented in client

--- src/core/ui.collection.js
+++ src/core/ui.collection.js
@@ -22,60 +22,71 @@ Mura.UI.Collection=Mura.UI.extend(
 	},
 
 	getCollection:function(){
-		this.context.source=this.context.source || '';
+		if(typeof this.context.feed != 'undefined' && typeof this.context.feed.getQuery != 'undefined'){
+			return this.context.feed.getQuery();
+		} else {
+			this.context.source=this.context.source || '';
 
-		if(typeof this.context.nextn != 'undefined'){
-			this.context.itemsperpage=this.context.nextn;
-		}
+			if(typeof this.context.nextn != 'undefined'){
+				this.context.itemsperpage=this.context.nextn;
+			}
 
-		if(typeof this.context.maxitems == 'undefined'){
-			this.context.maxitems=20;
-		}
+			if(typeof this.context.maxitems == 'undefined'){
+				this.context.maxitems=20;
+			}
 
-		if(typeof this.context.itemsperpage != 'undefined'){
-			this.context.itemsperpage=this.context.maxitems;
-		}
+			if(typeof this.context.itemsperpage != 'undefined'){
+				this.context.itemsperpage=this.context.nextn;
+			}
 
-		if(this.context.sourcetype=='relatedcontent'){
-			if(this.context.source=='custom'){
-				if(typeof this.context.items == 'array'){
-					this.context.items=this.context.items.join();
-				}
-				return Mura.get(Mura.apiEndpoint + '/?entityname=content&method=findMany&id=' + this.context.items,{
-					itemsperpage:this.context.itemsperpage,
-					maxitems:this.context.maxitems
-				}).then((response)=>{
-					return new Promise(function(resolve,reject) {
-							resolve(new Mura.EntityCollection(resp.data,self._requestcontext));
-					})
-				});
-			} else if(this.context.source=='reverse'){
-				return Mura.getEntity('content')
-					.set({
-						'contentid':Mura.contentid,
-						'id':Mura.contentid
-					}).getRelatedContent('reverse',{
-						itemsperpage:this.context.itemsperpage,
-						maxitems:this.context.maxitems,
-						sortby:this.context.sortby
-					})
-			} else {
-				return Mura.getEntity('content')
-					.set({
-						'contentid':Mura.contentid,
-						'id':Mura.contentid
-					}).getRelatedContent(this.context.source,{
+			if(this.context.sourcetype=='relatedcontent'){
+				if(this.context.source=='custom'){
+					if(typeof this.context.items == 'array'){
+						this.context.items=this.context.items.join();
+					}
+					return Mura.get(Mura.apiEndpoint + '/?entityname=content&method=findMany&id=' + this.context.items,{
 						itemsperpage:this.context.itemsperpage,
 						maxitems:this.context.maxitems
-					})
-			}
-		} else {
-			return Mura.getFeed('content')
+					}).then((response)=>{
+						return new Promise(function(resolve,reject) {
+								resolve(new Mura.EntityCollection(resp.data,self._requestcontext));
+						})
+					});
+				} else if(this.context.source=='reverse'){
+					return Mura.getEntity('content')
+						.set({
+							'contentid':Mura.contentid,
+							'id':Mura.contentid
+						}).getRelatedContent('reverse',{
+							itemsperpage:this.context.itemsperpage,
+							maxitems:this.context.maxitems,
+							sortby:this.context.sortby
+						})
+				} else {
+					return Mura.getEntity('content')
+						.set({
+							'contentid':Mura.contentid,
+							'id':Mura.contentid
+						}).getRelatedContent(this.context.source,{
+							itemsperpage:this.context.itemsperpage,
+							maxitems:this.context.maxitems
+						})
+				}
+			} else if(this.context.sourcetype=='children'){
+				return Mura.getFeed('content')
+					.where()
+					.prop('parentid').isEQ(Mura.contentid)
+					.maxItems(100)
+					.itemsPerPage(this.context.itemsperpage)
+					.getQuery();
+			} else {
+				return Mura.getFeed('content')
 					.where()
 					.prop('feedid').isEQ(this.context.source)
 					.maxItems(this.context.maxitems)
 					.itemsPerPage(this.context.itemsperpage)
 					.getQuery();
+			}
 		}
 	},
 
@@ -107,4 +118,4 @@ Mura.UI.Collection=Mura.UI.extend(
 	}
 });
 
-Mura.DisplayObject.Collection=Mura.UI.Collection;
+Mura.Module.Collection=Mura.UI.Collection;
